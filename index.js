94488fb471cb917f661892232069347c3f738e39
amonhaupt <amon.haupt@aol.com>
2026-01-29 16:59:26 +0100

    removed link to not used /messages endpoint

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use("/auth", require("./routes/auth"));
 app.use("/users", verifyToken, require("./routes/users"));
 //app.use("/contacts", verifyToken, require("./routes/contacts"));
 //app.use("/blocks", verifyToken, require("./routes/blocks"));
-app.use("/messages", verifyToken, require("./routes/messages")); // testing
+//app.use("/messages", verifyToken, require("./routes/messages")); // testing
 app.use("/conversations", verifyToken, require("./routes/conversations"));
 app.use("/groups", verifyToken, require("./routes/groups"));
 //app.use("/", require("./routes/"));
