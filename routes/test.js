abef8859ad3a72a9aa4d342b10c2beec7a5c3658
amonhaupt <amon.haupt@aol.com>
2026-02-18 08:08:38 +0100

    added test endpoint to check currently logged in user

--- routes/test.js
+++ routes/test.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const verifyToken = require('../middleware/verifyToken');
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.json({ 
     message: "Test API working!", 
+    timestamp: new Date().toISOString(),
+    status: "success"
+  });
+});
+
+
+
+router.get("/", verifyToken, (req, res) => {
+  res.json({ 
+    message: "Test Current User?", 
     currentUser: req.user,
     timestamp: new Date().toISOString(),
     status: "success"
